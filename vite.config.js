045a63908ab5eb197e9065461b7072156ac644a2
dyl124 <142150017+dyl124@users.noreply.github.com>
2024-01-29 21:16:46 +1100

    complete

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
-  }
+  },
+    build: {
+      rollupOptions: {
+        external: ['react-router-dom']
+    }
+    }
+  
 });
